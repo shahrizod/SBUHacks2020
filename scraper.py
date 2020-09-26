c4eea2f94a967f71f9b89dd0301bb86306d7b7b3
Shahrizod <shahrizod>
2020-09-25 23:25:11 -0400

    finished scraper

--- scraper.py
+++ scraper.py
@@ -6,13 +6,14 @@ import pandas as pd
 
 options = webdriver.ChromeOptions()
 #options.add_argument('headless')
-driver = webdriver.Chrome(options=options)  
+driver = webdriver.Chrome(options=options)
 url = "https://quizlet.com/130934620/print"
 driver.get(url)
 WebDriverWait(driver, 10).until(lambda x: x.find_element_by_class_name('PrintPageOptions-radioWrap'))
 driver.find_element_by_xpath('/html/body/div[3]/main/div/div/div/div[3]/div[1]/div/div[2]/div[2]/div[4]/label/input').click()
-
+WebDriverWait(driver, 10).until(lambda x: x.find_element_by_class_name('PrintPageOptions-radioWrap'))
 soup = BeautifulSoup(driver.page_source, 'html.parser')
+driver.quit()
 firstCol = soup.find_all(class_='term inner')
 secondCol = soup.find_all(class_ = 'def inner')
 
@@ -20,8 +21,10 @@ terms = []
 definitions = []
 
 for term in firstCol:
-    terms.append(term.text.lstrip('.'))
+    terms.append(term.text.split(".",1)[1])
 for definition in secondCol:
     definitions.append(definition.text)
 
-print(terms, definitions)
+data = {'Terms':terms,'Definitions':definitions}
+df = pd.DataFrame(data, columns=['Terms', 'Definitions'])
+print(df)
